5710a07e20f90d05f6fb03576132cdebfcfbf663
Dodo-1989 <naderzaeri89@gmail.com>
2020-02-01 13:52:23 -0600

    saving work

--- exercises/216-number-arrays.js
+++ exercises/216-number-arrays.js
@@ -1,6 +1,14 @@
 // Write a function "max" that takes an array of numbers returns the highest
 // number in the array.
 
+function max (arr){
+     if ( arr.length === 0 ){
+         return 0
+     }
+    // arr.sort(function(a,b){return a - b})
+    // return arr.pop()
+    return Math.max(...arr)
+}
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -9,7 +17,13 @@
 // Example:
 // sumNumbers([1, 4, 8]) --> 13
 
-
+function sumNumbers (arr){
+    let sum = 0;
+    for (let i in arr){
+        sum += arr[i]
+    }
+    return sum
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "positives" which is given an array of numbers and
@@ -18,7 +32,17 @@
 // positives([1, -3, 5, -3, 0]) --> [1, 5]
 // positives([1, 2, 3]) --> [1, 2, 3]
 // positives([-1, -2, -3]) --> []
-
+ function positives (arr){
+  var positiveNums = []
+  var negativeNums = []
+  for ( let i = 0 ; i < arr.length ; i ++){
+      if ( arr[i] > 0){
+          positiveNums.push(arr[i])
+      }
+      negativeNums.push(arr[i])
+  }
+  return positiveNums
+ }
 
 
 
@@ -27,14 +51,27 @@
 // array containing only the even numbers in the given array.
 // Hint: you may want to re-use your "isEven" function from 01-predicate-functions.js
 
-
+function evens (arr){
+    var isEvenArr = []
+    for ( let i in arr){
+        arr[i] % 2 === 0? isEvenArr.push(arr[i]) : isEvenArr.push('')
+    }
+    return isEvenArr.filter(x => x !== '')
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "odds" which takes an array of numbers and returns a new
 // array containing only the odd numbers in the given array.
 // Hint: you may want to re-use your "isOdd" function from 01-predicate-functions.js
 
-
+ function odds (arr){
+    var isOddArr = []
+    for ( let i in arr){
+        arr[i] % 2 !== 0? isOddArr.push(arr[i]) : isOddArr.push('')
+    }
+    return isOddArr.filter(x => x !== '')
+}
+ 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "integers" which takes an array of numbers and returns a new
@@ -44,7 +81,13 @@
 // Example:
 // integers([3.14, 2.4, 7, 8.1, 2]) --> [7, 2]
 
-
+function integers (arr){
+    let onlyInteger = []
+     for ( let j = 0 ; j < arr.length ; j++){
+         Number.isInteger(arr[j])? onlyInteger.push(arr[j]) : arr[j];
+     }
+     return onlyInteger
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "squareDance" which takes an array of numbers and returns a
@@ -52,3 +95,10 @@
 //
 // Example:
 // squareDance([1, 2, 3]) --> [1, 4, 9]
+function squareDance (arr){
+    var squared = []
+     for ( let i in arr){
+         squared.push(Math.pow(arr[i], 2))
+     }
+     return squared
+}
--- exercises/220-cities.js
+++ exercises/220-cities.js
@@ -7,9 +7,48 @@
 // ];
 // and returns a new array containing only those cities whose temperature is
 // cooler than 70 degrees.
+const cities1 = [
+       { name: 'Los Angeles', temperature: 60.0},
+       { name: 'Atlanta', temperature: 52.0 },
+       { name: 'Detroit', temperature: 48.0 },
+       { name: 'New York', temperature: 80.0 }
+     ];
 
-
+     function coolCities (arr){
+       //  const tempOnly = []
+         const cityNames = []
+       //  const indxs = Object.keys(cities1)
+          for ( let i in Object.keys(cities1)){
+               if (cities1[i].temperature < 70){
+                   cityNames.push(cities1[i])
+               }
+           }
+        //    for ( let j in tempOnly){
+        //        if (tempOnly[j] < 70){
+        //            cityNames.push(cities1[j])
+        //        }
+            
+          return cityNames
+        }
+    
+    
+    
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "cityNames" which takes an array of city objects like the
 // above problem and returns an array of the cities names.
+const cities = [
+       { name: 'Los Angeles', temperature: 60.0},
+       { name: 'Atlanta', temperature: 52.0 },
+       { name: 'Detroit', temperature: 48.0 },
+       { name: 'New York', temperature: 80.0 }
+     ];
+
+     function cityNames (arr){
+         var arrNames = []
+         const idx =  Object.keys(cities)
+          for ( let i in idx){
+              arrNames.push(cities[i].name)
+          }
+          return arrNames
+     }
